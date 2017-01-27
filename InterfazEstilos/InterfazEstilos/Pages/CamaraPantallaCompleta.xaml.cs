4a3677a6ff8851d2bc5d8783bd00dc188dadfad3
David Benítez <dbenitez96@outlook.es>
2017-01-27 14:22:37 +0100

    fhxt

--- InterfazEstilos/InterfazEstilos/Pages/CamaraPantallaCompleta.xaml.cs
+++ InterfazEstilos/InterfazEstilos/Pages/CamaraPantallaCompleta.xaml.cs
@@ -5,12 +5,14 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=234238
@@ -22,9 +24,46 @@ namespace InterfazEstilos.Pages
     /// </summary>
     public sealed partial class CamaraPantallaCompleta : Page
     {
+        SystemNavigationManager systemNavigationManager;
+
         public CamaraPantallaCompleta()
         {
             this.InitializeComponent();
+        }
+
+        private void appointmentsDetail_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            // Mark event as handled so we don't get bounced out of the app.
+            e.Handled = true;
+            OnBackRequested();
+        }
+        private void OnBackRequested()
+        {
+            // Page above us will be our master view.
+            // Make sure we are using the "drill out" animation in this transition.
+            if (Frame.CanGoBack)
+            {
+                Frame.GoBack(new SuppressNavigationTransitionInfo());
+            }
+            else
+            {
+                systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+            }
+        }
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            systemNavigationManager = SystemNavigationManager.GetForCurrentView();
+            systemNavigationManager.BackRequested -= appointmentsDetail_BackRequested;
+            systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            systemNavigationManager = SystemNavigationManager.GetForCurrentView();
+            systemNavigationManager.BackRequested += appointmentsDetail_BackRequested;
+            systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
         }
     }
 }
